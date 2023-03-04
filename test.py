18163fea67b61e2d1a8af31a583fc36248509a67
Dr. Emil A. Gazazyan <emil@quopt.net>
2023-03-04 10:26:16 +0400

    add plot  y=x^2

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-x=2
+x=np.arange(-5,5,0.1)
 
+y=np.power(x,2)
 
+plt.plot(x,y)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("y=x^2")
+plt.show()
 print(x)
